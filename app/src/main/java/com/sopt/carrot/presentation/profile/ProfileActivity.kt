11dc7fb5b333181f869c23bdbf36f93323ce5970
librarywon <librarywon@naver.com>
2023-05-18 19:01:05 +0900

    [Feat] #3-button auto On/Off, birthYearValidation,  validateRegister button

--- app/src/main/java/com/sopt/carrot/presentation/profile/ProfileActivity.kt
+++ app/src/main/java/com/sopt/carrot/presentation/profile/ProfileActivity.kt
@@ -1,12 +1,55 @@
 package com.sopt.carrot.presentation.profile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.sopt.carrot.R
+import com.sopt.carrot.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val viewModel: ProfileViewModel by viewModels()
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this // lifecycleOwner 설정
+
+        with(binding) {
+            // 초기값
+            btnProfileWoman.isSelected = true
+            btnProfileWoman.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
+            btnProfileAgreeAndApply.isEnabled = false
+        }
+
+        // isButtonEnabled를 옵저빙하여 UI 업데이트
+        viewModel.isButtonEnabled.observe(this, { isEnabled ->
+            binding.btnProfileAgreeAndApply.isEnabled = isEnabled
+        })
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        with(binding) {
+            btnProfileMan.setOnClickListener {
+                it.isSelected = true
+                btnProfileMan.setTextColor(ContextCompat.getColor(applicationContext,R.color.white))
+                btnProfileWoman.setTextColor(ContextCompat.getColor(applicationContext,R.color.color_sub_gray3))
+                btnProfileWoman.isSelected = false
+                viewModel?.selectedGender?.value = 0
+            }
+            btnProfileWoman.setOnClickListener {
+                it.isSelected = true
+                btnProfileWoman.setTextColor(ContextCompat.getColor(applicationContext,R.color.white))
+                btnProfileMan.setTextColor(ContextCompat.getColor(applicationContext,R.color.color_sub_gray3))
+                btnProfileMan.isSelected = false
+                viewModel?.selectedGender?.value = 1
+            }
+        }
     }
 }
