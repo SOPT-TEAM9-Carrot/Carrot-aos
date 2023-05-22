e396222129a207340401ea36cbb3c7fe9598cca3
Sohyun <so405@naver.com>
2023-05-22 12:22:59 +0900

    [FEAT] #2-connect Card adapter

--- app/src/main/java/com/sopt/carrot/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/sopt/carrot/presentation/home/HomeActivity.kt
@@ -1,4 +1,30 @@
 package com.sopt.carrot.presentation.home
 
-class HomeActivity {
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.carrot.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var adapter: CardAdapter
+    private val viewModel by viewModels<CardViewModel>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setAdapter()
+
+    }
+
+    private fun setAdapter() {
+        adapter = CardAdapter()
+        binding.rvHomeCard.adapter = adapter
+        adapter.submitList(viewModel.mockPopLists)
+    }
+
+
 }
