0cfaa6ca6f2ca8da9c5da95ad69646896df0d1df
Sohyun <so405@naver.com>
2023-05-24 01:14:31 +0900

    [CHORE] #14-change name

--- app/src/main/java/com/sopt/carrot/presentation/home/RecommendedJobAdapter.kt
+++ app/src/main/java/com/sopt/carrot/presentation/home/RecommendedJobAdapter.kt
@@ -7,13 +7,13 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.carrot.databinding.ItemHomeCardBinding
 
-class CardAdapter() : ListAdapter<Card, CardAdapter.CardAdapterViewHolder>(diffUtil) {
+class RecommendedJobAdapter() : ListAdapter<RecommendedJob, RecommendedJobAdapter.CardAdapterViewHolder>(diffUtil) {
 
 
     class CardAdapterViewHolder(private val binding: ItemHomeCardBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun onBind(data: Card) {
+        fun onBind(data: RecommendedJob) {
             with(binding) {
                 ivItemHomeCardImg.setImageDrawable(root.context.getDrawable(data.image))
                 tvItemHomeCardTitle.text = data.title
@@ -26,12 +26,12 @@ class CardAdapter() : ListAdapter<Card, CardAdapter.CardAdapterViewHolder>(diffU
 
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<Card>() {
-            override fun areItemsTheSame(oldItem: Card, newItem: Card): Boolean {
+        val diffUtil = object : DiffUtil.ItemCallback<RecommendedJob>() {
+            override fun areItemsTheSame(oldItem: RecommendedJob, newItem: RecommendedJob): Boolean {
                 return oldItem.id == newItem.id
             }
 
-            override fun areContentsTheSame(oldItem: Card, newItem: Card): Boolean {
+            override fun areContentsTheSame(oldItem: RecommendedJob, newItem: RecommendedJob): Boolean {
                 return oldItem == newItem
             }
 
--- app/src/main/java/com/sopt/carrot/presentation/home/RecommendedViewModel.kt
+++ app/src/main/java/com/sopt/carrot/presentation/home/RecommendedViewModel.kt
@@ -3,30 +3,30 @@ package com.sopt.carrot.presentation.home
 import androidx.lifecycle.ViewModel
 import com.sopt.carrot.R
 
-class CardViewModel : ViewModel() {
-    val mockCardLists = listOf(
-        Card(
+class RecommendedViewModel : ViewModel() {
+    val mockRecommendedJobLists = listOf(
+        RecommendedJob(
             id = 1,
             image = R.drawable.img_test_1,
             title = "당근 마켓",
             salary = "시급 10,000원"
 
         ),
-        Card(
+        RecommendedJob(
             id = 2,
             image = R.drawable.img_test_2,
             title = "당근 알바",
             salary = "시급 20,000원"
 
         ),
-        Card(
+        RecommendedJob(
             id = 3,
             image = R.drawable.img_test_1,
             title = "당근 마켓",
             salary = "시급 10,000원"
 
         ),
-        Card(
+        RecommendedJob(
             id = 4,
             image = R.drawable.img_test_2,
             title = "당근 알바",
