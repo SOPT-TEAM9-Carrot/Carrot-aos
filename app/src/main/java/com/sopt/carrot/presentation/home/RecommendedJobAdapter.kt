629f95055c7ae7f833d8a2790defd125ecc0c4a2
Sohyun <so405@naver.com>
2023-05-24 02:15:08 +0900

    [FIX] #14-fix name error

--- app/src/main/java/com/sopt/carrot/presentation/home/RecommendedJobAdapter.kt
+++ app/src/main/java/com/sopt/carrot/presentation/home/RecommendedJobAdapter.kt
@@ -5,19 +5,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.sopt.carrot.databinding.ItemHomeCardBinding
+import com.sopt.carrot.databinding.ItemHomeRecommendedJobBinding
 
-class RecommendedJobAdapter() : ListAdapter<RecommendedJob, RecommendedJobAdapter.CardAdapterViewHolder>(diffUtil) {
+class RecommendedJobAdapter() :
+    ListAdapter<RecommendedJob, RecommendedJobAdapter.CardAdapterViewHolder>(diffUtil) {
 
 
-    class CardAdapterViewHolder(private val binding: ItemHomeCardBinding) :
+    class CardAdapterViewHolder(private val binding: ItemHomeRecommendedJobBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun onBind(data: RecommendedJob) {
             with(binding) {
-                ivItemHomeCardImg.setImageDrawable(root.context.getDrawable(data.image))
-                tvItemHomeCardTitle.text = data.title
-                tvItemHomeCardSalary.text = data.salary
+                ivItemHomeRecommendedJobImg.setImageDrawable(root.context.getDrawable(data.image))
+                tvItemHomeRecommendedJobTitle.text = data.title
+                tvItemHomeRecommendedJobSalary.text = data.salary
             }
 
         }
@@ -27,11 +28,17 @@ class RecommendedJobAdapter() : ListAdapter<RecommendedJob, RecommendedJobAdapte
 
     companion object {
         val diffUtil = object : DiffUtil.ItemCallback<RecommendedJob>() {
-            override fun areItemsTheSame(oldItem: RecommendedJob, newItem: RecommendedJob): Boolean {
+            override fun areItemsTheSame(
+                oldItem: RecommendedJob,
+                newItem: RecommendedJob
+            ): Boolean {
                 return oldItem.id == newItem.id
             }
 
-            override fun areContentsTheSame(oldItem: RecommendedJob, newItem: RecommendedJob): Boolean {
+            override fun areContentsTheSame(
+                oldItem: RecommendedJob,
+                newItem: RecommendedJob
+            ): Boolean {
                 return oldItem == newItem
             }
 
@@ -39,7 +46,7 @@ class RecommendedJobAdapter() : ListAdapter<RecommendedJob, RecommendedJobAdapte
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardAdapterViewHolder {
-        val binding = ItemHomeCardBinding.inflate(
+        val binding = ItemHomeRecommendedJobBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
