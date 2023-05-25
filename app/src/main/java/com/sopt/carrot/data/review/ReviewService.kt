3b57f406ae92a3423711a7d659266a0750b53ac4
librarywon <librarywon@naver.com>
2023-05-26 00:30:34 +0900

    [CHORE] #9-setting for Intent

--- app/src/main/java/com/sopt/carrot/data/review/ReviewService.kt
+++ app/src/main/java/com/sopt/carrot/data/review/ReviewService.kt
@@ -2,14 +2,15 @@ package com.sopt.carrot.data.review
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 
+
 interface ReviewService {
+    @Headers("Authorization: 1") // 정적 헤더 설정
     @GET("/employer/{userId}/reviews?size=10")
     fun getReviewList(
-        @Header("Authorization") Authorization: String = "1",
-        @Path(value = "userId") userId: Int = 3
+        @Path(value = "userId") userId: Long //userId 받아올준비 완료
     ): Call<ResponseReviewDto>
 }
