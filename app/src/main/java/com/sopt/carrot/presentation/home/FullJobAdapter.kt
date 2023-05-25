0100ab32b8243e789ea27e8d0f27f37bf7b1a7cb
Sohyun <so405@naver.com>
2023-05-25 20:59:57 +0900

    [FEAT] #14-connect full job adapter to rv_home_full

--- app/src/main/java/com/sopt/carrot/presentation/home/FullJobAdapter.kt
+++ app/src/main/java/com/sopt/carrot/presentation/home/FullJobAdapter.kt
@@ -9,8 +9,9 @@ import com.bumptech.glide.Glide
 import com.sopt.carrot.data.home.ResponseFullListDto
 import com.sopt.carrot.databinding.ItemHomeFullJobBinding
 
-class FullJobAdapter() :
+class FullJobAdapter(state: Int) :
     ListAdapter<ResponseFullListDto.Detail.Post, FullJobAdapter.JobAdapterViewHolder>(diffUtil) {
+    private val recyclerViewDivision: Int = state
 
     class JobAdapterViewHolder(private val binding: ItemHomeFullJobBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -24,6 +25,7 @@ class FullJobAdapter() :
             }
 
         }
+
 
     }
 
@@ -47,7 +49,14 @@ class FullJobAdapter() :
         }
     }
 
-    override fun getItemCount(): Int = 3
+
+    override fun getItemCount(): Int {
+        return if (recyclerViewDivision == 1) {
+            3
+        } else {
+            super.getItemCount()
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobAdapterViewHolder {
         val binding = ItemHomeFullJobBinding.inflate(
