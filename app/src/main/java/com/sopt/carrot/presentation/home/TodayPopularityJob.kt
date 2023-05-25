8ee3a27b5ecd87be5fcd0fdb73369a419ff115e1
Sohyun <so405@naver.com>
2023-05-25 20:32:07 +0900

    [FEAT] #14-add dummy data

---
+++ app/src/main/java/com/sopt/carrot/presentation/home/TodayPopularityJob.kt
@@ -0,0 +1,12 @@
+package com.sopt.carrot.presentation.home
+
+data class TodayPopularityJob(
+    val id: Int,
+    val ranking:String,
+    val title: String,
+    val detail: String,
+    val interest: String,
+    val check: String,
+    val salary: String
+
+)
