b27638b0c4caae13046dd9124786c341a59a7302
librarywon <librarywon@naver.com>
2023-05-26 13:43:11 +0900

    [FEAT] #8-apply profileService

--- app/src/main/java/com/sopt/carrot/data/ApiPool.kt
+++ app/src/main/java/com/sopt/carrot/data/ApiPool.kt
@@ -2,15 +2,18 @@ package com.sopt.carrot.data
 
 import com.sopt.carrot.BuildConfig.API_SERVER_URL
 import com.sopt.carrot.data.page.PageService
+import com.sopt.carrot.data.profile.ProfileService
 import com.sopt.carrot.data.review.ReviewService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 object ApiPool {
     val pageService = RetrofitPool.toApiServer.create(PageService::class.java)
-    val reviewService = RetrofitPool.toApiServer.create<ReviewService>(ReviewService::class.java)
+    val reviewService = RetrofitPool.toApiServer.create(ReviewService::class.java)
+    val profileService = RetrofitPool.toApiServer.create(ProfileService::class.java)
 }
 
 object RetrofitPool {
