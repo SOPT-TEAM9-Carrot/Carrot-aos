c9753b24c3199c15271db0809401c18a28c6cddd
librarywon <librarywon@naver.com>
2023-05-26 14:40:42 +0900

    [FEAT] #8-activity intent

--- app/src/main/java/com/sopt/carrot/presentation/profile/CustomPopupDialog.kt
+++ app/src/main/java/com/sopt/carrot/presentation/profile/CustomPopupDialog.kt
@@ -2,12 +2,14 @@ package com.sopt.carrot.presentation.profile
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.Gravity
 import android.view.ViewGroup
 import android.view.Window
 import com.sopt.carrot.databinding.LayoutProfileCustompopupBinding
+import com.sopt.carrot.presentation.page.PageActivity
 
 class CustomPopupDialog(context: Context) : Dialog(context) {
 
@@ -28,7 +30,11 @@ class CustomPopupDialog(context: Context) : Dialog(context) {
 
         setCanceledOnTouchOutside(true)
         binding.btnProfilePopupCancel.setOnClickListener { dismiss() }
-        binding.btnProfilePopupExit.setOnClickListener { dismiss() }
+        binding.btnProfilePopupExit.setOnClickListener {
+            //dismiss()
+            val intent = Intent(context, PageActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
 
